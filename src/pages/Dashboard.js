accc2311daeb17a90d4e6cf734438cec5bd26191
Kluddizz <flhansen.93@gmail.com>
2020-05-27 12:32:35 +0200

    Add some cards to dashboard with dummy data

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,141 @@
 import React, { useContext, useEffect } from "react";
 import AppContext from "../contexts/AppContext";
 
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    height: "100%"
+  },
+
+  cardContent: {
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingBottom: 0
+  }
+}));
+
+const tableHeaders = [
+  { title: "Anlage" },
+  { title: "Kunde" },
+  { title: "Termin" },
+  { title: "Mitarbeiter" },
+  { title: "Status" }
+];
+
+const maintenances = [
+  {
+    id: 0,
+    system: "Kläranlage Eiderstedt",
+    customer: "DHSV Eiderstedt",
+    dueDate: "05.05.2020",
+    employee: "Max Mustermann",
+    state: 2
+  },
+  {
+    id: 1,
+    system: "Kläranlage Eiderstedt",
+    customer: "DHSV Eiderstedt",
+    dueDate: "05.05.2020",
+    employee: "Max Mustermann",
+    state: 1
+  },
+  {
+    id: 2,
+    system: "Kläranlage Eiderstedt",
+    customer: "DHSV Eiderstedt",
+    dueDate: "05.05.2020",
+    employee: "Max Mustermann",
+    state: 0
+  },
+  {
+    id: 3,
+    system: "Kläranlage Eiderstedt",
+    customer: "DHSV Eiderstedt",
+    dueDate: "05.05.2020",
+    employee: "Max Mustermann",
+    state: 0
+  },
+  {
+    id: 4,
+    system: "Kläranlage Eiderstedt",
+    customer: "DHSV Eiderstedt",
+    dueDate: "05.05.2020",
+    employee: "Max Mustermann",
+    state: 0
+  }
+];
+
 const Dashboard = ({ ...props }) => {
-	const [, setTitle] = useContext(AppContext);
+  const classes = useStyles();
+  const [, setTitle] = useContext(AppContext);
 
-	useEffect(() => {
-		setTitle("Dashboard");
-	}, [setTitle]);
+  useEffect(() => {
+    setTitle("Dashboard");
+  }, [setTitle]);
 
-	return <div></div>;
+  return (
+    <div>
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <Card className={classes.card}>
+            <CardHeader title="Übersicht" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={8}>
+          <Card className={classes.card}>
+            <CardHeader title="Anstehende Wartungen" />
+
+            <CardContent className={classes.cardContent}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    {tableHeaders.map((header, idx) => {
+                      return <TableCell key={idx}>{header.title}</TableCell>;
+                    })}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {maintenances.map(maintenance => (
+                    <TableRow key={maintenance.id}>
+                      <TableCell>{maintenance.system}</TableCell>
+                      <TableCell>{maintenance.customer}</TableCell>
+                      <TableCell>{maintenance.dueDate}</TableCell>
+                      <TableCell>{maintenance.employee}</TableCell>
+                      <TableCell>{maintenance.status}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      labelRowsPerPage="Zeilen pro Seite"
+                      rowsPerPageOptions={[5, 10, 25]}
+                      count={maintenances.length}
+                      rowsPerPage={5}
+                      page={0}
+                    />
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Dashboard;
