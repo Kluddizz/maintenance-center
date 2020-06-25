a8c7c6a67211d1331ec5166fdf7055c1ddd39402
Kluddizz <flhansen.93@gmail.com>
2020-06-25 15:58:25 +0200

    Add table card

--- src/pages/Maintenances.js
+++ src/pages/Maintenances.js
@@ -1,14 +1,30 @@
 import React, { useContext, useEffect } from "react";
 import AppContext from "../contexts/AppContext";
 
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+
 const Maintenances = ({ ...props }) => {
-	const [, setTitle] = useContext(AppContext);
+  const [, setTitle] = useContext(AppContext);
 
-	useEffect(() => {
-		setTitle("Wartungen");
-	}, [setTitle]);
+  useEffect(() => {
+    setTitle("Wartungen");
+  }, [setTitle]);
 
-	return <div></div>;
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={12}>
+          <Card>
+            <CardHeader title="Wartungen" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Maintenances;
