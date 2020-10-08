d70ff530c177f3fb5c7d416ea9504d5b838b66a2
Kluddizz <flhansen.93@gmail.com>
2020-10-08 15:05:48 +0200

    Implement feature to add new maintenances.

--- src/pages/Maintenances.js
+++ src/pages/Maintenances.js
@@ -1,27 +1,43 @@
 import React, { useContext, useEffect } from "react";
+import { useSnackbar } from "notistack";
+
+import Database from "../services/Database";
 import AppContext from "../contexts/AppContext";
+import AuthContext from "../contexts/AuthContext";
 import SystemContext from "../contexts/SystemContext";
 import MaintenanceContext from "../contexts/MaintenanceContext";
 import UserContext from "../contexts/UserContext";
 
 import StateChip from "../components/StateChip";
-
 import ExtendedTable from "../components/ExtendedTable";
 import Grid from "@material-ui/core/Grid";
 
 const Maintenances = ({ ...props }) => {
+  const {
+    token: [token],
+  } = useContext(AuthContext);
+  const [maintenances, updateMaintenances] = useContext(MaintenanceContext);
+  const [systems] = useContext(SystemContext);
+  const [users] = useContext(UserContext);
+  const [, setTitle] = useContext(AppContext);
+  const { enqueueSnackbar } = useSnackbar();
+
   const handleAdd = async (item) => {
-    console.log(item);
+    const response = await Database.createMaintenance(token, item);
+
+    if (response.success) {
+      enqueueSnackbar("Neue Wartung wurde eingetragen", { variant: "success" });
+      updateMaintenances();
+    } else {
+      enqueueSnackbar("Wartung konnte nicht eingetragen werden", {
+        variant: "error",
+      });
+    }
   };
 
   const handleEdit = async (item) => {};
 
   const handleDelete = async (item) => {};
-
-  const [maintenances, updateMaintenances] = useContext(MaintenanceContext);
-  const [systems] = useContext(SystemContext);
-  const [users] = useContext(UserContext);
-  const [, setTitle] = useContext(AppContext);
 
   useEffect(() => {
     setTitle("Wartungen");
@@ -35,13 +51,29 @@ const Maintenances = ({ ...props }) => {
             title="Wartungen"
             items={maintenances}
             headers={[
-              { name: "Anlage", field: "systemid" },
+              { name: "Beschreibung", field: "name" },
+              {
+                name: "Anlage",
+                render: (item) =>
+                  systems.find((s) => s.id === item.systemid)?.name,
+              },
               { name: "Kunde", field: "customerid" },
               { name: "Termin", field: "dueDate" },
-              { name: "Mitarbeiter", field: "userid" },
+              {
+                name: "Mitarbeiter",
+                render: (item) => {
+                  const user = users.find((u) => u.id === item.userid);
+                  return `${user?.firstname} ${user?.lastname}`;
+                },
+              },
               { name: "Status", field: "stateid" },
             ]}
             itemFields={[
+              {
+                name: "name",
+                description: "Beschreibung",
+                type: "string",
+              },
               {
                 name: "systemid",
                 description: "Anlage",
